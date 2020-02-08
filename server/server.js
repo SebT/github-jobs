e3647eb9e6771db5bab36ed863da373f84207665
Sébastien TOUR <sebastien@beekast.com>
2020-02-08 14:12:18 +0100

    feat(server): serve vue app from node

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const fastify = require("fastify");
+const fastifyStatic = require("fastify-static");
 const axios = require("axios");
 const querystring = require("querystring");
+const path = require("path");
 
 const GITHUB_JOBS_API_URL = "https://jobs.github.com/positions.json";
 
-module.exports = function({ port }) {
-  const app = fastify({ logger: true });
-
+function setupGithubJobsProxy(app) {
   app.get("/api/jobs", async (request, reply) => {
     const proxiedUrl =
       GITHUB_JOBS_API_URL + "?" + querystring.stringify(request.query);
@@ -15,6 +15,17 @@ module.exports = function({ port }) {
     const { data: jobs } = await axios.get(proxiedUrl);
     reply.send(jobs);
   });
+}
+
+function setupStaticFilesServer(app) {
+  app.register(fastifyStatic, { root: path.join(__dirname, "../dist") });
+}
+
+module.exports = function({ port }) {
+  const app = fastify({ logger: true });
+
+  setupGithubJobsProxy(app);
+  setupStaticFilesServer(app);
 
   return app.listen(port);
 };
